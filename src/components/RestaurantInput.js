0bb7b6cda640190cc9e48f92c8bf74a766e4f638
nmhummel <natalie.hummel@gmail.com>
2021-03-12 14:14:58 -0600

    commit tree a8464c3ce893305aa3f0e66fdcdd266a9abf8a34

--- src/components/RestaurantInput.js
+++ src/components/RestaurantInput.js
@@ -23,7 +23,7 @@ export class RestaurantInput extends Component {
 
   handleOnSubmit = event => {
     event.preventDefault();
-    // add missing code
+    this.props.addRestaurant(this.state);
   }
 
   render() {
@@ -50,5 +50,17 @@ export class RestaurantInput extends Component {
 };
 
 
-//connect this component by wrapping RestaurantInput below
-export default RestaurantInput
+const mapDispatchToProps = dispatch => {
+  return {
+    addRestaurant: (newRestaurant) => {
+      dispatch(addRestaurant(newRestaurant))
+    }
+  };
+};
+
+
+export default connect(null, mapDispatchToProps)(RestaurantInput);
+
+// Alternative form:
+
+// export default connect(null, { addRestaurant })(RestaurantInput);
